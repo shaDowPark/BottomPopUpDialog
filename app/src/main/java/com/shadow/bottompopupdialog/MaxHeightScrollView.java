9b3b50559f49776c5f115f81a3184031e956d24c
Zhongwy <805580462@qq.com>
2016-06-20 11:09:28 +0800

    init

--- app/src/main/java/com/shadow/bottompopupdialog/MaxHeightScrollView.java
+++ app/src/main/java/com/shadow/bottompopupdialog/MaxHeightScrollView.java
@@ -2,6 +2,7 @@ package com.shadow.bottompopupdialog;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.widget.ScrollView;
 
 /**
@@ -22,11 +23,20 @@ public class MaxHeightScrollView extends ScrollView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         Context context = getContext();
-        if(null != context) {
-            int screenHeight = ScreenUtils.getScreenHeight(context);
+        if (null != context) {
+            int screenHeight = getScreenHeight(context);
             heightMeasureSpec = MeasureSpec.makeMeasureSpec(screenHeight * 2 / 3, MeasureSpec.AT_MOST);
         }
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
+
+    /**
+     * 获取屏幕高度
+     */
+    private int getScreenHeight(Context context) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return metrics.heightPixels;
+    }
+
 
 }
